ad9a101af90ca060449e35a3145e424f0bc51060
kim-doan <doan960105@gmail.com>
2021-05-03 00:12:15 +0900

    FirstCommit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,39 @@
+import './polyfills';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { 
+  BindCodeNode,
+  BindStopCodeNode,
+  BindBadCodeNode,
+  BindFailCodeNode
+} from './common/CodePool/CodePool'
+
+import {
+  BindItemNode,
+  BindProductNode,
+  BindMaterialNode,
+  BindToolNode,
+  BindPartNode,
+  BindProcessNode,
+  BindUserNode,
+  BindCompanyNode
+} from './common/MasterPool/MasterPool'
+
+BindCodeNode()
+BindStopCodeNode()
+BindBadCodeNode()
+BindFailCodeNode()
+BindItemNode()
+BindProductNode()
+BindMaterialNode()
+BindToolNode()
+BindPartNode()
+BindProcessNode()
+BindUserNode()
+BindCompanyNode()
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +42,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
